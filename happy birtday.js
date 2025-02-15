f1826b7639a79a0cc8575c0c5dc946af151ddaa7
kash <valdezkyleashley15@gmail.com>
2025-02-15 13:13:57 +0800

    Update happy birtday.js

--- happy birtday.js
+++ happy birtday.js
@@ -5,7 +5,7 @@ let w = (c.width = window.innerWidth),
 (hh = h / 2),
   (opts = {
     // change the text in here //
-    strings: ["HAPPY", "BIRTHDAY!", "Charmaine", "Shanghai kuma"],
+    strings: ["HAPPY", "BIRTHDAY!", "Charmaine", "Shanghaikuma"],
     charSize: 30,
     charSpacing: 35,
     lineHeight: 40,
